3e592ae5c00eedeeae84fb028e6d6850142f003c
Talha Sijad <talha.sijad@JDMBUR1074.local>
2021-06-07 15:45:06 +0100

    Made some changes.

--- serverless-with-front-end/handler.js
+++ serverless-with-front-end/handler.js
@@ -1,16 +1,66 @@
-const AWS = require('aws-sdk');
-const client = new AWS.DynamoDB.DocumentClient({ region: 'us-east-2' });
+const AWS = require("aws-sdk");
+const dynamo = new AWS.DynamoDB.DocumentClient();
 
-module.exports.helloWorld = (event, context, callback) => {
-  let scanningParams = {
-    TableName: 'Names'
+exports.handler = async (event, context) => {
+  let body;
+  let statusCode = 200;
+  const headers = {
+    "Content-Type": "application/json"
+  };
+
+  try {
+    switch (event.routeKey) {
+      case "DELETE /items/{id}":
+        await dynamo
+          .delete({
+            TableName: "Names",
+            Key: {
+              id: event.pathParameters.id
+            }
+          })
+          .promise();
+        body = `Deleted item ${event.pathParameters.id}`;
+        break;
+      case "GET /items/{id}":
+        body = await dynamo
+          .get({
+            TableName: "Names",
+            Key: {
+              id: event.pathParameters.id
+            }
+          })
+          .promise();
+        break;
+      case "GET /items":
+        body = await dynamo.scan({ TableName: "http-crud-tutorial-items" }).promise();
+        break;
+      case "PUT /items":
+        let requestJSON = JSON.parse(event.body);
+        await dynamo
+          .put({
+            TableName: "Names",
+            Item: {
+              id: requestJSON.id,
+              price: requestJSON.price,
+              name: requestJSON.name
+            }
+          })
+          .promise();
+        body = `Put item ${requestJSON.id}`;
+        break;
+      default:
+        throw new Error(`Unsupported route: "${event.routeKey}"`);
+    }
+  } catch (err) {
+    statusCode = 400;
+    body = err.message;
+  } finally {
+    body = JSON.stringify(body);
   }
 
-  client.scan(scanningParams, function(err, data) {
-    if (err) {
-      callback(err, null);
-    } else {
-      callback(null, data);
-    }
-  });
+  return {
+    statusCode,
+    body,
+    headers
+  };
 };
