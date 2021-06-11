ad3cf3ce8235f8ac4540800617471c1b79c40d94
Talha Sijad <talha.sijad@jdmbur1074.home>
2021-06-11 12:26:58 +0100

    Made some changes.

--- serverless-with-front-end/handler.js
+++ serverless-with-front-end/handler.js
@@ -1,55 +1,93 @@
 const AWS = require("aws-sdk");
 const dynamo = new AWS.DynamoDB.DocumentClient();
+const dotenv = require ("dotenv");
+
+dotenv.config();
 
 exports.handler = async (event, context) => {
   let body;
   let statusCode = 200;
   const headers = {
     "Content-Type": "application/json"
-  };
+  }; 
+
+  // try {
+  //   switch (event.routeKey) {
+  //     case "DELETE /items/{id}":
+  //       await dynamo
+  //         .delete({
+  //           TableName: "Names",
+  //           Key: {
+  //             id: event.pathParameters.id
+  //           }
+  //         })
+  //         .promise();
+  //       body = `Deleted item ${event.pathParameters.id}`;
+  //       break;
+  //     case "GET /items/{id}":
+  //       body = await dynamo
+  //         .get({
+  //           TableName: "Names",
+  //           Key: {
+  //             id: event.pathParameters.id
+  //           }
+  //         })
+  //         .promise();
+  //       break;
+  //     case "GET /items":
+  //       body = await dynamo.scan({ TableName: "Names" }).promise();
+  //       break;
+  //     case "PUT /items":
+  //       let requestJSON = JSON.parse(event.body);
+  //       await dynamo
+  //         .put({
+  //           TableName: "Names",
+  //           Item: {
+  //             id: requestJSON.id,
+  //             price: requestJSON.price,
+  //             name: requestJSON.name
+  //           }
+  //         })
+  //         .promise();
+  //       body = `Put item ${requestJSON.id}`;
+  //       break;
+  //     default:
+  //       throw new Error(`Unsupported route: "${event.routeKey}"`);
+  //   }
+  // } catch (err) {
+  //   statusCode = 400;
+  //   body = err.message;
+  // } finally {
+  //   body = JSON.stringify(body);
+  // }
 
   try {
     switch (event.routeKey) {
-      case "DELETE /items/{id}":
-        await dynamo
-          .delete({
-            TableName: "Names",
-            Key: {
-              id: event.pathParameters.id
-            }
-          })
-          .promise();
-        body = `Deleted item ${event.pathParameters.id}`;
+      case "GET /names":
+        body = 'You called the GET method';
         break;
-      case "GET /items/{id}":
-        body = await dynamo
-          .get({
-            TableName: "Names",
-            Key: {
-              id: event.pathParameters.id
-            }
-          })
-          .promise();
+      case "GET /names/{id}":
+        body = 'You called the GET/{id} method';
         break;
-      case "GET /items":
-        body = await dynamo.scan({ TableName: "http-crud-tutorial-items" }).promise();
+      case "PUT /names":
+        body = 'You called the PUT method';
         break;
-      case "PUT /items":
-        let requestJSON = JSON.parse(event.body);
-        await dynamo
-          .put({
-            TableName: "Names",
-            Item: {
-              id: requestJSON.id,
-              price: requestJSON.price,
-              name: requestJSON.name
-            }
-          })
-          .promise();
-        body = `Put item ${requestJSON.id}`;
-        break;
+      // case "PUT /items":
+      //   let requestJSON = JSON.parse(event.body);
+      //   await dynamo
+      //     .put({
+      //       TableName: "Names",
+      //       Item: {
+      //         id: requestJSON.id,
+      //         price: requestJSON.price,
+      //         name: requestJSON.name
+      //       }
+      //     })
+      //     .promise();
+      //   body = `Put item ${requestJSON.id}`;
+      //   break;
       default:
-        throw new Error(`Unsupported route: "${event.routeKey}"`);
+        throw new Error(`imvalid route: "${event.routeKey}"`);
     }
   } catch (err) {
     statusCode = 400;
@@ -64,3 +102,25 @@ exports.handler = async (event, context) => {
     headers
   };
 };
+
+$(document).ready(function () {
+  let accessKey = process.env.ACCESS_KEY
+  alert(accessKey);
+})
+
+var apigClient = apigClientFactory.newClient({
+  accessKey: process.env.ACCESS_KEY,
+  secretKey: process.env.SECURITY_ACCESS_ID,
+});
+
+$('#submitName').click(function() {
+  $.ajax({
+      url: "https://oezfcv9lqf.execute-api.us-east-2.amazonaws.com/Development",
+      success: function(data) {
+          $('#names').html('');
+          $.each(data.Items, function(key, value) {
+              $('#names').append('<p>' + this.name + '</p>');
+          });
+      }
+  });
+});
