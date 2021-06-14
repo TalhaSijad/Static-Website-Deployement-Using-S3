8ede7e970f5707f2862d465297317a7cbda40f4b
Talha Sijad <talha.sijad@jdmbur1074.home>
2021-06-14 12:46:43 +0100

    Made some changes.

--- serverless-with-front-end/handler.js
+++ serverless-with-front-end/handler.js
@@ -8,83 +8,48 @@ exports.handler = async (event, context) => {
     "Content-Type": "application/json"
   }; 
 
-  // try {
-  //   switch (event.routeKey) {
-  //     case "DELETE /items/{id}":
-  //       await dynamo
-  //         .delete({
-  //           TableName: "Names",
-  //           Key: {
-  //             id: event.pathParameters.id
-  //           }
-  //         })
-  //         .promise();
-  //       body = `Deleted item ${event.pathParameters.id}`;
-  //       break;
-  //     case "GET /items/{id}":
-  //       body = await dynamo
-  //         .get({
-  //           TableName: "Names",
-  //           Key: {
-  //             id: event.pathParameters.id
-  //           }
-  //         })
-  //         .promise();
-  //       break;
-  //     case "GET /items":
-  //       body = await dynamo.scan({ TableName: "Names" }).promise();
-  //       break;
-  //     case "PUT /items":
-  //       let requestJSON = JSON.parse(event.body);
-  //       await dynamo
-  //         .put({
-  //           TableName: "Names",
-  //           Item: {
-  //             id: requestJSON.id,
-  //             price: requestJSON.price,
-  //             name: requestJSON.name
-  //           }
-  //         })
-  //         .promise();
-  //       body = `Put item ${requestJSON.id}`;
-  //       break;
-  //     default:
-  //       throw new Error(`Unsupported route: "${event.routeKey}"`);
-  //   }
-  // } catch (err) {
-  //   statusCode = 400;
-  //   body = err.message;
-  // } finally {
-  //   body = JSON.stringify(body);
-  // }
-
   try {
     switch (event.routeKey) {
-      case "GET /names":
-        body = 'You called the GET method';
+      case "DELETE /names/{id}":
+        await dynamo
+          .delete({
+            TableName: "Names",
+            Key: {
+              id: event.pathParameters.id
+            }
+          })
+          .promise();
+        body = `Deleted name with ID ${event.pathParameters.id}`;
         break;
-      case "GET /names/{id}":
-        body = 'You called the GET/{id} method';
+      case "GET /name/{id}":
+        body = await dynamo
+          .get({
+            TableName: "Names",
+            Key: {
+              id: event.pathParameters.id
+            }
+          })
+          .promise();
+        break;
+      case "GET /names":
+        body = await dynamo.scan({ TableName: "Names" }).promise();
         break;
       case "PUT /names":
-        body = 'You called the PUT method';
+        let requestJSON = JSON.parse(event.body);
+        await dynamo
+          .put({
+            TableName: "Names",
+            Item: {
+              id: requestJSON.id,
+              price: requestJSON.price,
+              name: requestJSON.name
+            }
+          })
+          .promise();
+        body = `Put name ${requestJSON.id}`;
         break;
-      // case "PUT /items":
-      //   let requestJSON = JSON.parse(event.body);
-      //   await dynamo
-      //     .put({
-      //       TableName: "Names",
-      //       Item: {
-      //         id: requestJSON.id,
-      //         price: requestJSON.price,
-      //         name: requestJSON.name
-      //       }
-      //     })
-      //     .promise();
-      //   body = `Put item ${requestJSON.id}`;
-      //   break;
       default:
-        throw new Error(`imvalid route: "${event.routeKey}"`);
+        throw new Error(`Unsupported route: "${event.routeKey}"`);
     }
   } catch (err) {
     statusCode = 400;
@@ -92,10 +57,4 @@ exports.handler = async (event, context) => {
   } finally {
     body = JSON.stringify(body);
   }
-
-  return {
-    statusCode,
-    body,
-    headers
-  };
-};
+}
